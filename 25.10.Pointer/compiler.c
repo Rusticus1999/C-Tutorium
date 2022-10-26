f1ba44aeeeb1fc10a3905f78a8e435ddd12cbafd
Nicholas <nicholas.schuran@gmail.com>
2022-10-26 14:02:40 +0200

    Tutorium 25.10.

---
+++ 25.10.Pointer/compiler.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*
+Aufgabe:
+Kompiliere diese Datei mit dem gcc und Stoppe den compiler nach jedem Zwischenschritt.
+Nutze das ergebnis des vorherigen Zwischenschritts, um den nächsten schritt zu Produzieren.
+1. Präprozessor (-E)
+2. Compiler (-S)
+3. Assembler (-c)
+4. Linker
+
+*/
+
+int main(int argc, char* argv[]){
+
+printf("\nHallo Welt\n");
+
+return 0;
+}
---
+++ 25.10.Pointer/test.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+void tausche(int, int);
+/*
+Aufgabe 1:
+Deklarariere und initialisiere die 2 int variablen a und b mit 1 und 2
+Deklariere die Funktion tausche
+
+Aufgabe 2:
+Implementiere die Funktion tausche.
+Sie bekommt die 2 variablen in main Übergeben und Tauscht deren Rückgabewerte.
+
+Entwickle erst einen Ansatz. Was könnte Schiefgehen?
+
+Aufgabe 3:
+Allokiere und räume den Speicher in main um deinen Namen global zu Speichern. Welchen Datentypen braucht dieses Array?
+Schreibe eine Funktion invert die den namen umdreht.
+
+Aufgabe 4:
+Schreibe einen Passwortgenerator benutze dazu rand() und int('0').
+
+Aufgabe 5:
+Smart Home
+In einem smarten Haushalt gibt es 8 Lampen die ein und ausgeschaltet werden müssen:
+
+Schreibe Funktionen um folgende Funktionalitäten zu implementieren:
+-Ausgabe der Lampen in der Konsole als serie von 0en und 1en.
+-Ein schalten einzelner Lampen.
+-Ausschalten einzelner Lampen.
+-Umschalten einzelner Lampen.
+-Umschalten aller Lampen ohne nutzung der vorherigen Funktion.
+-Lauflicht rechtsrum oder linksrum.
+
+*/
+
+int main(int argc, char* argv[]){
+
+printf("Basic\n");
+
+tausche(1, 2);
+
+return 0;
+}
+
+void tausche(int a, int b){
+	printf("a = %d, b = %d\n", a,b);
+	int c = 0;
+	c = a;
+	a = b;
+	b = c;
+	printf("a = %d, b = %d\n", a,b);
+
+}
